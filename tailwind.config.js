0b2e2748f77cb26eb38fdf99ae971f5b25cffbc4
sai2021krishna <saikrishna.at2021@gmail.com>
2024-06-23 06:54:19 -0700

    HYA-6 | cleanup activity

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,26 +33,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-/*Slate
- Gray
- Zinc
- Neutral
- Stone
- Red
- Orange
- Amber
- Yellow
- Lime
- Green
- Emerald
- Teal
- Cyan
- Sky
- Blue
- Indigo
- Violet
- Purple
- Fuchsia
- Pink
- Rose */
