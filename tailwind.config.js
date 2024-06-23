620394bf6e42e508993b2cd2c41d9fa47fc46a6c
sai2021krishna <saikrishna.at2021@gmail.com>
2024-06-23 06:30:58 -0700

    KAN-8 | Tailwind integrated

---
+++ tailwind.config.js
@@ -0,0 +1,58 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      slate: colors.slate,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
+  },
+  plugins: [],
+};
+
+/*Slate
+ Gray
+ Zinc
+ Neutral
+ Stone
+ Red
+ Orange
+ Amber
+ Yellow
+ Lime
+ Green
+ Emerald
+ Teal
+ Cyan
+ Sky
+ Blue
+ Indigo
+ Violet
+ Purple
+ Fuchsia
+ Pink
+ Rose */
